6a18903749158f34cec5b6b9255f2fd6fa0b2be9
Vitefait productions <vitefait.maintenance@gmail.com>
2020-06-07 14:04:47 +0200

    First person controller first commit

---
+++ First person controller/FirstPersonMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class FirstPersonMovement : MonoBehaviour
+{
+    public float speed = 5;
+    Vector2 velocity;
+
+
+    void Start()
+    {
+        // Lock the cursor.
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        velocity.y = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        velocity.x = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        transform.Translate(velocity.x, 0, velocity.y);
+
+        // Turn on the cursor when the user presses escape.
+        if (Input.GetKeyDown(KeyCode.Escape))
+            Cursor.lockState = CursorLockMode.None;
+    }
+}
